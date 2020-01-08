d490f2bc9fd6e573cccf5334f1b47bcd3646bda9
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-08 09:32:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -29,14 +29,14 @@ end
 # using director_data as input
 def gross_for_director(director_data)
     pp director_data
-      
+
     result = {}
     movie_index = 0
-      
+
     while movie_index < director_data[:movies].length do
       result[] +=  director_data[:movies][movie_index][:worldwide_gross]
       movie_index += 1
     end
-    
+
     return result
   end
