51581403ee0e0df3044efc9cdffe07876237cce9
Jennifer Beaver <jen.lee.beaver@gmail.com>
2020-01-08 09:36:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,7 +28,7 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-    pp director_data
+    #pp director_data
 
     result = {}
     movie_index = 0
